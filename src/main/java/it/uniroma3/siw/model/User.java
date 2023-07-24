6f12489315ae3ef7e36a7c60e7b8f67f05f0e203
bianccc01 <giorgiobiancini@gmail.com>
2023-07-24 17:56:34 +0200

    Prima prova

---
+++ src/main/java/it/uniroma3/siw/model/User.java
@@ -0,0 +1,59 @@
+package it.uniroma3.siw.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users") // cambiamo nome perchè in postgres user e' una parola riservata
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+	@NotNull
+	private String name;
+	@NotNull
+	private String surname;
+	@NotNull
+	private String email;
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
