201a68b2c7dde7eb898d52efb29ebb2f5bc4eb9b
alessandradb <ale.delbuono1@gmail.com>
2023-07-25 14:09:22 +0200

    Prima prova

--- src/main/java/it/uniroma3/siw/model/Categoria.java
+++ src/main/java/it/uniroma3/siw/model/Categoria.java
@@ -1,13 +1,13 @@
 package it.uniroma3.siw.model;
 
 import java.util.List;
+
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 
