94637c21cccd798b9b1ff49e2f920290ac016a54
alessandradb <ale.delbuono1@gmail.com>
2023-07-27 17:22:01 +0200

    Lavoro su categoria

--- src/main/java/it/uniroma3/siw/controller/CategoriaController.java
+++ src/main/java/it/uniroma3/siw/controller/CategoriaController.java
@@ -1,9 +1,15 @@
 package it.uniroma3.siw.controller;
 
+import javax.validation.Valid;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import it.uniroma3.siw.model.Categoria;
 import it.uniroma3.siw.service.CategoriaService;
@@ -13,6 +19,8 @@ public class CategoriaController {
 	
 	@Autowired 
 	private CategoriaService categoriaService;
+	@Autowired
+	//private CategoriaValidator categoriaValidator;
 	
 	@GetMapping("/")
 	public String index() {
@@ -30,6 +38,26 @@ public class CategoriaController {
 		model.addAttribute("categorie",this.categoriaService.allCategorie());
 		return "categorie.html";
 	}
+	
+	@PostMapping("/newCategoria")
+	public String newCategoria(@Valid @ModelAttribute("categoria") Categoria categ, BindingResult bindingResult, Model model) {
+		
+		//this.categoriaValidator.validate(categ, bindingResult);
+		if (!bindingResult.hasErrors()) {
+			this.categoriaService.saveCategoria(categ); 
+			model.addAttribute("categoria", categ);
+			return "index.html";
+		} else {
+			return "/formNewCategoria.html"; 
+		}
+	}
+	
+	
+	
+	
+	//rimuovi categoria
+	//aggiungi destinazione
+	//rimuovi destinazione
 	
 	
 
