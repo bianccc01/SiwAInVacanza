d9a8a3db4f87ef8c3b7fee3b6a20f7fc8547fb51
alessandradb <ale.delbuono1@gmail.com>
2023-08-14 14:54:46 +0200

    aggiustamenti validatori

--- src/main/java/it/uniroma3/siw/controller/validation/CategoriaValidator.java
+++ src/main/java/it/uniroma3/siw/controller/validation/CategoriaValidator.java
@@ -1,13 +1,14 @@
 package it.uniroma3.siw.controller.validation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import it.uniroma3.siw.model.Categoria;
 import it.uniroma3.siw.repository.CategoriaRepository;
 
-
+@Component
 public class CategoriaValidator implements Validator {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
--- src/main/java/it/uniroma3/siw/controller/validation/DestinazioneValidator.java
+++ src/main/java/it/uniroma3/siw/controller/validation/DestinazioneValidator.java
@@ -1,13 +1,14 @@
 package it.uniroma3.siw.controller.validation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import it.uniroma3.siw.model.Destinazione;
 import it.uniroma3.siw.repository.DestinazioneRepository;
 
-
+@Component
 public class DestinazioneValidator implements Validator {
 	@Autowired
 	private DestinazioneRepository destinazioneRepository;
