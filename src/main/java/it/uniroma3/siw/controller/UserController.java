16084177e2adf4efca8860272d775d1091fb0e24
alessandradb <ale.delbuono1@gmail.com>
2023-08-17 16:02:24 +0200

    userController

---
+++ src/main/java/it/uniroma3/siw/controller/UserController.java
@@ -0,0 +1,30 @@
+package it.uniroma3.siw.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import it.uniroma3.siw.model.User;
+import it.uniroma3.siw.service.UserService;
+
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/userDetails/{id}")
+	public String getUserDetails(@PathVariable("id") Long id, Model model) {
+		User user = this.userService.getUser(id);
+		if(user!=null)
+			model.addAttribute("user", user);
+		return "paginaUtente.html";
+		
+	}
+
+
+}
