fe2b8385d81d8a3ef9b3baf5e383def0785908a5
alessandradb <ale.delbuono1@gmail.com>
2023-09-10 14:17:58 +0200

    user e credentials validator

---
+++ src/main/java/it/uniroma3/siw/controller/validation/UserValidator.java
@@ -0,0 +1,28 @@
+package it.uniroma3.siw.controller.validation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.repository.CredentialsRepository;
+
+@Component
+public class UserValidator implements Validator {
+	@Autowired
+	private CredentialsRepository credentialsRepository;
+
+	@Override
+	public void validate(Object o, Errors errors) {
+		Credentials cred = (Credentials)o;
+		if (cred.getUsername()!=null && credentialsRepository.existsByUsername(cred.getUsername())) {
+			errors.reject("credentials.duplicate");
+		}
+	}
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return Credentials.class.equals(aClass);
+	}
+
+}
